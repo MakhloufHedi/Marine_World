5b78a9d223d9348552ca8318ead12a6195a9d890
montassar123456 <montassar.benbrahim@ensi-uma.tn>
2022-08-18 10:42:37 +0100

    add new

---
+++ app/src/main/java/com/example/summer_project/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.summer_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SpaceShooter2(this));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/com/example/summer_project/MainActivity3.java
@@ -0,0 +1,24 @@
+package com.example.summer_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SpaceShooter3(this));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
