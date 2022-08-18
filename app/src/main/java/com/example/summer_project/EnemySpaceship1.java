366e43b03b387ccdea8227dee51aeab519c8facb
montassar123456 <montassar.benbrahim@ensi-uma.tn>
2022-08-18 10:36:39 +0100

    add new

---
+++ app/src/main/java/com/example/summer_project/EnemySpaceship1.java
@@ -0,0 +1,36 @@
+package com.example.summer_project;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
+
+public class EnemySpaceship1 {
+    Context context;
+    Bitmap enemySpaceship1;
+    int ex, ey;
+    int enemyVelocity;
+    Random random;
+
+    public EnemySpaceship1(Context context) {
+        this.context = context;
+        enemySpaceship1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.rocket2);
+        random = new Random();
+        ex = 200 + random.nextInt(400);
+        ey = 0;
+        enemyVelocity = 14 + random.nextInt(10);
+    }
+
+    public Bitmap getEnemySpaceship1(){
+        return enemySpaceship1;
+    }
+
+    int getEnemySpaceshipWidth1(){
+        return enemySpaceship1.getWidth();
+    }
+
+    int getEnemySpaceshipHeight1(){
+        return enemySpaceship1.getHeight();
+    }
+}
---
+++ app/src/main/java/com/example/summer_project/EnemySpaceship3.java
@@ -0,0 +1,36 @@
+package com.example.summer_project;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
+
+public class EnemySpaceship3 {
+    Context context;
+    Bitmap enemySpaceship3;
+    int ex, ey;
+    int enemyVelocity;
+    Random random;
+
+    public EnemySpaceship3(Context context) {
+        this.context = context;
+        enemySpaceship3 = BitmapFactory.decodeResource(context.getResources(), R.drawable.rocket2);
+        random = new Random();
+        ex = 600 + random.nextInt(800);
+        ey = 0;
+        enemyVelocity = 14 + random.nextInt(10);
+    }
+
+    public Bitmap getEnemySpaceship3(){
+        return enemySpaceship3;
+    }
+
+    int getEnemySpaceshipWidth3(){
+        return enemySpaceship3.getWidth();
+    }
+
+    int getEnemySpaceshipHeight3(){
+        return enemySpaceship3.getHeight();
+    }
+}
