87bed196484b41032b5340a3ebb9cf12cd94bfcc
mohamedaloulou <110423979+mohamedaloulou@users.noreply.github.com>
2022-09-17 13:13:59 +0100

    Update QuestionsList.java

--- app/src/main/java/com/example/summer_project/QuestionsList.java
+++ app/src/main/java/com/example/summer_project/QuestionsList.java
@@ -1,7 +1,7 @@
 package com.example.summer_project;
 
 public class QuestionsList {
-
+// la class quesyionList qui contient comme attribut question option 1 2 3 et 4 et la reponse correcte et la reponse selectionne
     private String question, option1, option2, option3, option4,answer;
     private String userSelectedAnswer;
 
