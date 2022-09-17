3336b2259dfadba1cea85ddc4bf00225dafbaab2
Hedi Makhlouf <94899721+MakhloufHedi@users.noreply.github.com>
2022-09-17 15:48:01 +0100

    Update MenuActivity2.java

--- app/src/main/java/com/example/summer_project/MenuActivity2.java
+++ app/src/main/java/com/example/summer_project/MenuActivity2.java
@@ -3,11 +3,12 @@ package com.example.summer_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MenuActivity2 extends AppCompatActivity {
+    
+     private SoundPlayer soundPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,32 @@ public class MenuActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_menu2);
 
         ImageView back_button2 = findViewById(R.id.back_button2);
+        ImageView level1shooter = findViewById(R.id.level1shooter);
+        ImageView level2shooter = findViewById(R.id.level2shooter);
+        ImageView level3shooter = findViewById(R.id.level3shooter);
+
+        soundPlayer = new SoundPlayer(this);
+
+        level1shooter.setOnClickListener(view -> {
+            Intent intent = new Intent(MenuActivity2.this ,StartUp1.class);
+            startActivity(intent);
+            soundPlayer.playButtonShooterSound();
+            overridePendingTransition(R.anim.static_animation,R.anim.zoom_in);
+        });
+
+        level2shooter.setOnClickListener(view -> {
+            Intent intent = new Intent(MenuActivity2.this ,StartUp2.class);
+            startActivity(intent);
+            soundPlayer.playButtonShooterSound();
+            overridePendingTransition(R.anim.static_animation,R.anim.zoom_in);
+        });
+
+        level3shooter.setOnClickListener(view -> {
+            Intent intent = new Intent(MenuActivity2.this ,StartUp3.class);
+            startActivity(intent);
+            soundPlayer.playButtonShooterSound();
+            overridePendingTransition(R.anim.static_animation,R.anim.zoom_in);
+        });
 
         back_button2.setOnClickListener(view -> {
             Intent intent = new Intent(MenuActivity2.this ,MainActivity.class);
