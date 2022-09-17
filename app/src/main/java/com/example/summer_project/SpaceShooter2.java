9327383f625160da4efe1e9d57a784e48ac973e3
Hedi Makhlouf <94899721+MakhloufHedi@users.noreply.github.com>
2022-09-17 15:55:54 +0100

    Update SpaceShooter2.java

--- app/src/main/java/com/example/summer_project/SpaceShooter2.java
+++ app/src/main/java/com/example/summer_project/SpaceShooter2.java
@@ -61,7 +61,7 @@ public class SpaceShooter2 extends View {
         ourSpaceship2 = new OurSpaceship2(context);
         enemySpaceship2 = new EnemySpaceship2(context);
         handler = new Handler();
-        background = BitmapFactory.decodeResource(context.getResources(), R.drawable.game);
+        background = BitmapFactory.decodeResource(context.getResources(), R.drawable.shooter_back);
         lifeImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.life);
         scorePaint = new Paint();
         scorePaint.setColor(Color.RED);
