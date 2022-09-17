a021012230ec2c5628cb2dafafb928a139955da7
Hedi Makhlouf <94899721+MakhloufHedi@users.noreply.github.com>
2022-09-17 15:44:52 +0100

    Update MainActivity1.java

--- app/src/main/java/com/example/summer_project/MainActivity1.java
+++ app/src/main/java/com/example/summer_project/MainActivity1.java
@@ -1,11 +1,8 @@
 package com.example.summer_project;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.airbnb.lottie.LottieAnimationView;
 public class MainActivity1 extends AppCompatActivity {
 
     @Override
